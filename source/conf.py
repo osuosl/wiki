afe25602b7e0cd20c75d2b12213cb241f9c4acc6
Khuong Luu <6059347+khashf@users.noreply.github.com>
2019-09-20 10:42:29 -0700

    Remove u'' prefix since it's default in python3

--- source/conf.py
+++ source/conf.py
@@ -45,8 +45,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'OSL Wiki'
-copyright = u'2014, Oregon State University'
+project = 'OSL Wiki'
+copyright = '2014, Oregon State University'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -200,8 +200,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'OSLWiki.tex', u'OSL Wiki Documentation',
-   u'OSU Open Source Lab', 'manual'),
+  ('index', 'OSLWiki.tex', 'OSL Wiki Documentation',
+   'OSU Open Source Lab', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -230,8 +230,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'oslwiki', u'OSL Wiki Documentation',
-     [u'OSU Open Source Lab'], 1)
+    ('index', 'oslwiki', 'OSL Wiki Documentation',
+     ['OSU Open Source Lab'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -244,8 +244,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'OSLWiki', u'OSL Wiki Documentation',
-   u'OSU Open Source Lab', 'OSLWiki', 'One line description of project.',
+  ('index', 'OSLWiki', 'OSL Wiki Documentation',
+   'OSU Open Source Lab', 'OSLWiki', 'One line description of project.',
    'Miscellaneous'),
 ]
 
