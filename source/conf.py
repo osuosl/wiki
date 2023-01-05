767bf5d6bdd1a2d4bc6015e2e67ae7f27b9c0403
Zachary Wallace <zwall@osuosl.org>
2023-01-05 13:32:35 -0800

    Changed copyright date, weird build error

--- source/conf.py
+++ source/conf.py
@@ -46,7 +46,7 @@ main_doc = 'index'
 
 # General information about the project.
 project = 'OSL Wiki'
-copyright = '2014-2020, Oregon State University'
+copyright = '2014-2023, Oregon State University'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
