121c38af0bc8bc4d8cc37e9266a58341b0cafcc4
Lance Albertson <lance@osuosl.org>
2023-01-19 13:39:05 -0800

    Enable sphinx.ext.autosectionlabel
    This will automatically create labels for the first two levels of headers of
    every document. This will prepend the document path when you want to reference a
    label.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,22 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.graphviz',         # https://www.sphinx-doc.org/en/master/usage/extensions/graphviz.html
+    'sphinx.ext.autosectionlabel', # https://www.sphinx-doc.org/en/master/usage/extensions/autosectionlabel.html
+]
+
+# True to prefix each section label with the name of the document it is in,
+# followed by a colon. For example, index:Introduction for a section called
+# Introduction that appears in document index.rst. Useful for avoiding
+# ambiguity when the same section heading appears in different documents.
+autosectionlabel_prefix_document = True
+
+# If set, autosectionlabel chooses the sections for labeling by its depth. For
+# example, when set 1 to autosectionlabel_maxdepth, labels are generated only
+# for top level sections, and deeper sections are not labeled. It defaults to
+# None (disabled).
+autosectionlabel_maxdepth = 2
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
